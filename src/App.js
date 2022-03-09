8b3f8b9a4a4642d85b362af36a7f2095ffbff736
rakibhossain431 <rakibjp431@gmail.com>
2022-03-10 03:05:01 +0600

    t-smart

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Header from './component/Layout/Header';
+
+function App() {
+  return (
+    <div className="wrapper fixed__footer">
+       <Header />
+    </div>
+  );
+}
+
+export default App;
